c86f51a9e2993fde9418fb6a5204a1e110ee29f8
yichunzhao <zhaoyichun@hotmail.com>
2020-07-18 20:21:39 +0200

    transfer data from controller to view, via a Model.

--- src/main/java/com/ynz/front/demothymeleaf/controllers/FrontController.java
+++ src/main/java/com/ynz/front/demothymeleaf/controllers/FrontController.java
@@ -1,13 +1,35 @@
 package com.ynz.front.demothymeleaf.controllers;
 
+import com.ynz.front.demothymeleaf.domain.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller
-@RequestMapping("/")
+
 public class FrontController {
 
-    String home() {
+    @GetMapping("/")
+    public String home() {
         return "index";
     }
+
+    @PostMapping("/hello")
+    public String hello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("greetingPersonName", name);
+        return "hello";
+    }
+
+    @GetMapping("/students")
+    public String getStudents(Model model) {
+        Student student1 = Student.builder().studentId(1).name("Daoqi Yang").build();
+        Student student2 = Student.builder().studentId(2).name("Haley Jena").build();
+        model.addAttribute("students", Arrays.asList(student1, student2));
+        return "students";
+    }
+
 }
---
+++ src/main/java/com/ynz/front/demothymeleaf/domain/Student.java
@@ -0,0 +1,15 @@
+package com.ynz.front.demothymeleaf.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private Integer studentId;
+    private String name;
+}
