2dbc53c78e13db29c76845d34e8ecd96c3142f8a
yichunzhao <zhaoyichun@hotmail.com>
2020-07-18 22:47:46 +0200

    for each

--- src/main/java/com/ynz/front/demothymeleaf/controllers/FrontController.java
+++ src/main/java/com/ynz/front/demothymeleaf/controllers/FrontController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
-
 public class FrontController {
 
     @GetMapping("/")
@@ -30,6 +34,13 @@ public class FrontController {
         Student student2 = Student.builder().studentId(2).name("Haley Jena").build();
         model.addAttribute("students", Arrays.asList(student1, student2));
         return "students";
+    }
+
+    @GetMapping("/names")
+    public String getNames(Model model) {
+        List<String> names = Stream.of("Mike","Mia","Chris","Evan","Merry").collect(toList());
+        model.addAttribute("names",names);
+        return "names";
     }
 
 }
