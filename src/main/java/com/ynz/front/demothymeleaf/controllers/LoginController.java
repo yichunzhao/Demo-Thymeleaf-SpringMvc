baa8eab3e32872f752c51559e7a90a7a4d35f080
yichunzhao <zhaoyichun@hotmail.com>
2020-08-12 13:27:47 +0200

    login validation field error msg

--- src/main/java/com/ynz/front/demothymeleaf/controllers/LoginController.java
+++ src/main/java/com/ynz/front/demothymeleaf/controllers/LoginController.java
@@ -6,9 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -23,8 +26,11 @@ public class LoginController {
     }
 
     @PostMapping("/submitLogin")
-    public String submitLogin(@ModelAttribute Login login, Model model) {
+    public String submitLogin(@Valid @ModelAttribute Login login, BindingResult result, Model model) {
         log.info("Accept login : " + login.toString());
+        if (result.hasErrors()) {
+            return "login";
+        }
 
         if (detailService.match(login)) {
             return "redirect:/showrooms";
