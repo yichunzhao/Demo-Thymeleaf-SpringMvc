41885672b285186a8ce5a2177e3081b27f6d2d02
yichunzhao <zhaoyichun@hotmail.com>
2020-08-13 12:45:22 +0200

    removing import

--- src/main/java/com/ynz/front/demothymeleaf/controllers/LogoutController.java
+++ src/main/java/com/ynz/front/demothymeleaf/controllers/LogoutController.java
@@ -4,7 +4,6 @@ package com.ynz.front.demothymeleaf.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
